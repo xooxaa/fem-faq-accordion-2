f17a7b8dd21526a7932193aebaa686c2f1df7a31
xooxaa <37028661+xooxaa@users.noreply.github.com>
2022-11-08 13:19:25 +0100

    2 files modified, 1 added, 3 deleted

---
+++ index.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+const faqs = document.querySelectorAll('.faqs');
+let activeFaq = undefined;
+let faq = undefined;
+
+const toggleVisibility = function (e) {
+  try {
+    const pathpath = e.path;
+
+    if (activeFaq) {
+      activeFaq.children[0].classList.remove('active');
+      activeFaq.children[1].classList.add('hide');
+    }
+
+    faq = pathpath[pathpath.length - 7];
+    faq.children[0].classList.add('active');
+    faq.children[1].classList.remove('hide');
+    activeFaq = faq;
+  } catch {}
+};
+
+for (let faq of faqs) {
+  faq.addEventListener('click', toggleVisibility);
+}
